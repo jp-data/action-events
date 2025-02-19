4f1b75dd3fe448e32295a30402b9a37871419cb3
joao paulo <jhonysp37@gmail.com>
2025-02-19 16:30:49 -0300

    ranking section on invite page

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import Logo from '../../assets/Logo.svg'
+import MedalGold from '../../assets/Medal_gold.svg'
+import MedalSilver from '../../assets/Medal_silver.svg'
+import MedalCooper from '../../assets/Medal_cooper.svg'
 import { InputField, InputIcon, InputRoot } from '@/components/Input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import { IconButton } from '@/components/IconButton'
@@ -73,6 +76,41 @@ export default function InvitePage() {
                             <Medal className='size-5 text-purple absolute top-3 left-3' />
                         </div>
 
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-full max-w-[440px] space-y-5'>
+                <h2 className='text-gray-200 text-xl font-heading leading-none'>
+                    Ranking de indicações
+                </h2>
+                <div className='space-y-4'>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                            <span className='font-semibold'>1º</span> | Nome teste
+                        </span>
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1030
+                        </span>
+                        <Image src={MedalGold} alt="gold_medal" className='absolute top-0 right-8' />
+                    </div>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                            <span className='font-semibold'>2º</span> | Nome teste
+                        </span>
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1030
+                        </span>
+                        <Image src={MedalSilver} alt="gold_medal" className='absolute top-0 right-8' />
+                    </div>
+                    <div className='relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
+                        <span className='text-sm text-gray-300 leading-none'>
+                            <span className='font-semibold'>3º</span> | Nome teste
+                        </span>
+                        <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>
+                            1030
+                        </span>
+                        <Image src={MedalCooper} alt="gold_medal" className='absolute top-0 right-8' />
                     </div>
                 </div>
             </div>
