7975f292d4d983e426e5fe556ee5e78ac1b324dd
joao paulo <jhonysp37@gmail.com>
2025-02-19 17:10:16 -0300

    separate components - home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
-import Logo from '../assets/Logo.svg'
-import { ArrowRight, Mail, Radio, User } from 'lucide-react';
-import { InputField, InputIcon, InputRoot } from '@/components/Input';
-import { Button } from '@/components/Button';
+import Logo from '../../assets/Logo.svg'
+import { Radio } from 'lucide-react';
+import { SubscriptionForm } from './subscription-form';
 
 export default function Home() {
   return (
@@ -30,27 +29,7 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h ás 21h | Online e gratuito
           </p>
         </div>
-        <form className='bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]'>
-          <h2 className='font-heading font-semibold text-gray-200 text-xl'>Inscrição</h2>
-          <div className='space-y-3'>
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type='text' placeholder='Nome completo' />
-            </InputRoot>
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type='email' placeholder='Email' />
-            </InputRoot>
-          </div>
-          <Button type='submit'>
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   );
